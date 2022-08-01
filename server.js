ec067e52dffb8e1c8f84142f22db19b11e88fe65
jay <jayleungg@gmail.com>
2022-08-01 09:49:00 -0400

    building out inital heroku connection

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//MIDDLEWARE
+app.use(express.static(`${__dirname}/client/build`));
+
+//ROUTEs
 app.get("/", (req, res) => {
   res.send("I have root");
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
 });
 
 app.listen(PORT, () => {
